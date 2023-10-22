bf610cdad2f1e4263908ece357b9bed1206a59df
jaeyoung <kjy0302014@gmail.com>
2023-10-22 12:32:20 +0900

    [chore] 임시 임시모듈 저장

---
+++ api/api-devwiki/src/main/java/com/devwiki/backend/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.devwiki.backend.config;
+
+/*import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}*/
